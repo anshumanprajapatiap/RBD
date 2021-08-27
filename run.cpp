cddae7632df6e01fde153ef845a9db2d879331b9
Anshuman Prajapati <44315336+anshumanprajapatiap@users.noreply.github.com>
2021-08-27 09:01:52 +0530

    a

--- run.cpp
+++ run.cpp
@@ -1,116 +1,117 @@
-/*
-1
-30
-17 24
-26 18
-26 34
-13 27
-2 10
-8 3
-20 11
-17 4
-7 21
-17 13
-9 12
-10 8
-21 23
-14 23
-26 28
-13 10
-19 23
-28 30
-17 34
-23 12
-4 30
-30 22
-8 25
-15 39
-1 36
-17 14
-21 12
-4 37
-21 23
-2 27
-*/
+// /*
+// 1
+// 30
+// 17 24
+// 26 18
+// 26 34
+// 13 27
+// 2 10
+// 8 3
+// 20 11
+// 17 4
+// 7 21
+// 17 13
+// 9 12
+// 10 8
+// 21 23
+// 14 23
+// 26 28
+// 13 10
+// 19 23
+// 28 30
+// 17 34
+// 23 12
+// 4 30
+// 30 22
+// 8 25
+// 15 39
+// 1 36
+// 17 14
+// 21 12
+// 4 37
+// 21 23
+// 2 27
+// */
 
-#include "bits-stdc++.h"
-using namespace std;
+// #include "bits-stdc++.h"
+// using namespace std;
 
-bool com(const pair<int,int> &a, const pair<int,int> &b){
+// bool com(const pair<int,int> &a, const pair<int,int> &b){
     
-       return a.first>b.first;
-}
+//        return a.first>b.first;
+// }
 
-bool com2(const pair<int,int> &a, const pair<int,int> &b){
+// bool com2(const pair<int,int> &a, const pair<int,int> &b){
     
-       return a.second>b.second;
-}
+//        return a.second>b.second;
+// }
 
-void pri(vector<pair<int, int>> v){
-    cout<<endl<<"here"<<endl;
-    for(auto e:v){
-        cout<<e.first<<" "<<e.second<<endl;
-    }
-    cout<<endl;
-}
+// void pri(vector<pair<int, int>> v){
+//     cout<<endl<<"here"<<endl;
+//     for(auto e:v){
+//         cout<<e.first<<" "<<e.second<<endl;
+//     }
+//     cout<<endl;
+// }
 
-int solve(int n, vector<pair<int, int>> &v){
+// int solve(int n, vector<pair<int, int>> &v){
     
     
-    sort(v.begin(), v.end(), com);
+//     sort(v.begin(), v.end(), com);
     
-    int h = v[0].second;
+//     int h = v[0].second;
     
-    int sum = h;
+//     int sum = h;
 
-    for(int i=1; i<n; i++){
-        if(h>v[i].second){
-            sum += v[i].second;
-            h = v[i].second;
-        }
-    }
+//     for(int i=1; i<n; i++){
+//         if(h>v[i].second){
+//             sum += v[i].second;
+//             h = v[i].second;
+//         }
+//     }
 
-    sort(v.begin(), v.end(), com2);
-    int r = v[0].first;
-    h = v[0].second;
+//     sort(v.begin(), v.end(), com2);
+//     int r = v[0].first;
+//     h = v[0].second;
 
-    int sum2 = h;
-    for(int i=1; i<n; i++){
-        if(r>v[i].first){
-            sum2 += v[i].second;
-            r = v[i].first;
-        }
-    }
-
-
-    return max(sum, sum2);
-}
+//     int sum2 = h;
+//     for(int i=1; i<n; i++){
+//         if(r>v[i].first){
+//             sum2 += v[i].second;
+//             r = v[i].first;
+//         }
+//     }
 
 
-int main()
-{
-    int t;
-    cin>>t;
-    while(t){
+//     return max(sum, sum2);
+// }
+
+
+// int main()
+// {
+//     int t;
+//     cin>>t;
+//     while(t){
         
-        int n;
-        cin>>n;
+//         int n;
+//         cin>>n;
         
-        vector<pair<int, int>> v;
+//         vector<pair<int, int>> v;
         
-        for(int i=0; i<n; i++){
-            int x,y;
-            cin>>x>>y;
+//         for(int i=0; i<n; i++){
+//             int x,y;
+//             cin>>x>>y;
             
-            v.push_back(make_pair(x,y));
-        }
+//             v.push_back(make_pair(x,y));
+//         }
         
         
-        int ans  = solve(n, v);
-        cout<<ans<<endl;
+//         int ans  = solve(n, v);
+//         cout<<ans<<endl;
         
-        t--;
-    }
+//         t--;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
